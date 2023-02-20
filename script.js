f764dbbdbf421c5b9466918c430b4340af362a36
LukaNikcevic77 <nikcevic.luka77@gmail.com>
2023-02-20 23:55:23 +0100

    Add js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const removeNewBook = document.querySelector('#removeNewBook');
+const newBookForm = document.querySelector('.BookForm');
+
+console.log(removeNewBook);
+console.log(newBookForm);
+
+removeNewBook.addEventListener('click', (e) => {
+  newBookForm.classList.add('startPopDown');
+  console.log(newBookForm);
+});
 
