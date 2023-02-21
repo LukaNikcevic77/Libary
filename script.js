618496194ef0c8332be0a72d12ccd186379d3803
LukaNikcevic77 <nikcevic.luka77@gmail.com>
2023-02-21 21:32:41 +0100

    Finish animations

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-const removeNewBook = document.querySelector('#removeNewBook');
-const newBookForm = document.querySelector('.BookForm');
+const removeNewBook = document.getElementById('removeNewBook');
+const addBookButton = document.getElementById('AddBook');
+const newBookForm = document.getElementById('BookForm');
+const removeOldBook = document.getElementById('removeOld');
+const oldBook = document.getElementById('OldBook');
+let canAddBook = true;
 
-console.log(removeNewBook);
-console.log(newBookForm);
 
 removeNewBook.addEventListener('click', (e) => {
-  newBookForm.classList.add('startPopDown');
-  console.log(newBookForm);
+  newBookForm.classList.add('Scale-Down-anim');
+
+  setTimeout(() => {
+    newBookForm.classList.remove('Scale-Down-anim');
+    newBookForm.style.visibility = 'hidden';
+    canAddBook = true;
+  }, 1000);
 });
 
+addBookButton.addEventListener('click', (e) => {
+  if (canAddBook == true) {
+    newBookForm.classList.add('Scale-Up-anim');
+    canAddBook = false;
+    newBookForm.style.visibility = 'none';
+    setTimeout(() => {
+      newBookForm.style.visibility = 'initial';
+      newBookForm.classList.remove('Scale-Up-anim');
+    }, 1000);
+  }
+});
+
+removeOldBook.addEventListener('click', (e) => {
+  oldBook.classList.add('Fade-Away-anim');
+
+  setTimeout(() => {
+    oldBook.classList.remove('Fade-Away-anim');
+    oldBook.remove(this);
+  }, 1000);
+});
